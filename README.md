889ae74189b4ff4ef90ded7a26da4b4b247b27db
Jon Wedaman <jwedaman@shoobx.com>
2015-08-17 11:05:32 -0400

    This seems to work

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # terraform-provider-simplescript
-Terraform plugin for providing values via script
+
+Terraform plugin for providing values via script.
+
+Here's an example terraform:
+
+    resource "simplescript_run" "test" {
+        command = "echo '{"test": "123"}'"
+    }
+    
+    output "cmd-output" {
+        value = "${simplescript_run.test.text_output}"
+    }
+    
+    output "cmd-json" {
+        value = "${simplescript_run.test.json_output.test}"
+    }
---
+++ resource.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+    "crypto/sha256"
+    "encoding/hex"
+    "os"
+    "os/exec"
+    "encoding/json"
+    "log"
+    "strings"
+    "path/filepath"
+    "github.com/hashicorp/terraform/helper/schema"
+)
+
+func resource() *schema.Resource {
+    return &schema.Resource{
+        Create: Create,
+        Delete: Delete,
+        Exists: Exists,
+        Read:   Read,
+
+        Schema: map[string]*schema.Schema{
+            "command": &schema.Schema{
+                Type:        schema.TypeString,
+                Required:    true,
+                Description: "command to run",
+                ForceNew:    true,
+                // Make a "best effort" attempt to relativize the file path.
+                StateFunc: func(v interface{}) string {
+                    pwd, err := os.Getwd()
+                    if err != nil {
+                        return v.(string)
+                    }
+                    rel, err := filepath.Rel(pwd, v.(string))
+                    if err != nil {
+                        return v.(string)
+                    }
+                    return rel
+                },
+            },
+            "json_output": &schema.Schema{
+                Type:        schema.TypeMap,
+                Computed:    true,
+                Description: "command output as json. nil if unparsable.",
+            },
+            "text_output": &schema.Schema{
+                Type:        schema.TypeString,
+                Computed:    true,
+                Description: "command output as string",
+            },
+        },
+    }
+}
+
+func Create(d *schema.ResourceData, meta interface{}) error {
+    command := d.Get("command").(string)
+    output, json_output := run_cmd(command)
+    d.Set("text_output", output)
+    d.Set("json_output", json_output)
+    d.SetId(hash(output))
+    return nil
+}
+
+func Delete(d *schema.ResourceData, meta interface{}) error {
+    d.SetId("")
+    return nil
+}
+
+func Exists(d *schema.ResourceData, meta interface{}) (bool, error) {
+    command := d.Get("command").(string)
+    output, _ := run_cmd(command)
+    return hash(output) == d.Id(), nil
+}
+
+func Read(d *schema.ResourceData, meta interface{}) error {
+    // Logic is handled in Exists, which only returns true if the rendered
+    // contents haven't changed. That means if we get here there's nothing to
+    // do.
+    return nil
+}
+
+func hash(s string) string {
+    sha := sha256.Sum256([]byte(s))
+    return hex.EncodeToString(sha[:])
+}
+
+func run_cmd(s string) (string, map[string]interface{}) {
+    ss := safeSplit(s)
+    cmdName := ss[0]
+    cmdArgs := ss[1:]
+    cmdOut, err := exec.Command(cmdName, cmdArgs...).Output()
+    if err != nil {
+        log.Println(err)
+        return "", nil
+    }
+    // attempt json decode
+    var m map[string]interface{}
+    err = json.Unmarshal(cmdOut, &m)
+    if err != nil{
+        log.Println(err)
+        m = nil
+    }
+    return string(cmdOut), m
+}
+
+// stealing this function for cmdline
+func safeSplit(s string) []string {
+	split := strings.Split(s, " ")
+
+	var result []string
+	var inquote string
+	var block string
+	for _, i := range split {
+		if inquote == "" {
+			if strings.HasPrefix(i, "'") || strings.HasPrefix(i, "\"") {
+				inquote = string(i[0])
+				block = strings.TrimPrefix(i, inquote) + " "
+			} else {
+				result = append(result, i)
+			}
+		} else {
+			if !strings.HasSuffix(i, inquote) {
+				block += i + " "
+			} else {
+				block += strings.TrimSuffix(i, inquote)
+				inquote = ""
+				result = append(result, block)
+				block = ""
+			}
+		}
+	}
+
+	return result
+}
